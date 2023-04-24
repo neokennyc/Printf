310ef225982a922a116ccbf1e9beb0149e88d551
neokennyc <kenechukwuneolla@gmail.com>
2023-04-24 07:44:49 -0700

    test

--- writeout.c
+++ writeout.c
@@ -4,13 +4,6 @@
 
 int writeout(char ch)
 {
-	static char buffer[BUFF_SIZE];
-int buffpos;
 
-buffpos = 0;
-
-buffer[buffpos++] = ch;
-		buffer[buffpos] = '\0';
-
-	return(write(1, &buffer[0], 1));
+	return(write(1, &ch, 1));
 }
